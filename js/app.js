4b9ef25b718db28853698974d12d5f19f5e08c7f
SandyPanG <soleibiza@hotmail.com>
2018-08-03 09:07:36 -0500

    entra a la data

--- js/app.js
+++ js/app.js
@@ -1,13 +1,38 @@
 $(document).ready(function(){
-	var dibujarPokes = function(data){
-			var name = "";
-			var url = "";
-			data.forEach(function(element) {
-					name = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
-					url = $("<a>").attr("id", pokemon.name).attr("href", "#").append($("<strong>").text(name));
-				$('#elementos').append(armarTemplate(name,url));
-			});
+	// var dibujarPokes = function(data){
+	// 		var name = "";
+	// 		var url = "";
+	// 		data.forEach(function(element) {
+	// 				name = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+	// 				url = $("<a>").attr("id", pokemon.name).attr("href", "#").append($("<strong>").text(name));
+	// 			$('#elementos').append(armarTemplate(name,url));
+	// 		});
+		$("#search-poke").click(function(event){
+			event.preventDefault();
+			console.log("entro");
+			$('#elementos').empty();
+			var gif = $('#poke-text').val();
+			console.log(gif);
+			ajaxPoke();
+		});
+		var ajaxPoke = function(poke){
+			$.ajax({
+				url: 'https://pokeapi.co/api/v2/pokemon/',
+				type: 'GET',
+				datatype: 'json',
+		})
+
+		.done(function(response){
+				var data = response;
+				console.log(response);
+				dibujarPokes(response);               
+		})
+
+		.fail(function(){
+				console.log(error);        
+		});
 	};
+	});
 
 
 	// var armarTemplate = function(name,url){
@@ -15,37 +40,35 @@ $(document).ready(function(){
 	// 		return t;
 	// };
 
-	var ajaxPoke = function(poke){
-		$.ajax({
-            url: 'https://pokeapi.co/api/v2/pokemon/',
-            type: "GET",
-            crossDomain: true,
-            data: JSON.stringify(somejson),
-            dataType: "json",
-            success: function (response) {
-                var resp = JSON.parse(response)
-                alert(resp.status);
-            },
-            error: function (xhr, status) {
-                alert("error");
-            }
-        })
-			// $.ajax({
-			// 		url: 'http://pokeapi.co/api/v2/pokedex/1/',
-			// 		type: 'GET',
-			// 		datatype: 'json',
-			// 		data: {'limit' : '12'}
-			// })
+	// var ajaxPoke = function(poke){
+	// 	$.ajax({
+    //         url: 'https://pokeapi.co/api/v2/pokemon/',
+    //         type: "GET",
+    //         dataType: "json",
+    //         success: function (response) {
+    //             var resp = JSON.parse(response)
+    //             alert(resp.status);
+    //         },
+    //         error: function (xhr, status) {
+    //             alert("error");
+    //         }
+    //     })
+		// 	$.ajax({
+		// 			url: 'http://pokeapi.co/api/v2/pokemon/',
+		// 			type: 'GET',
+		// 			datatype: 'json',
+		// 	})
 	
-// 			.done(function(response){
-// 					console.log(response);
-// 					dibujarPokes(response.data);               
-// 			})
+		// 	.done(function(response){
+		// 			var data = response;
+		// 			console.log(response);
+		// 			// dibujarPokes(response.data);               
+		// 	})
 
-// 			.fail(function(){
-// 					console.log(error);        
-// 			});
-// 	}
+		// 	.fail(function(){
+		// 			console.log(error);        
+		// 	});
+		// };
 	
 
 // 	$("#search-poke").click(function(event){
